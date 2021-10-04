6df021feddd96908f7db364a619dc34f94d75f23
copydataai <copydataai@gmail.com>
2021-10-04 18:17:14 -0400

    Define ConfigModule(simple)

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { CreateUserDto } from '../dtos/users.dtos';
 
 import { User } from '../entities/user.entity';
@@ -8,7 +9,7 @@ import { ProductsService } from '../../products/services/products.service';
 export class UsersService {
   constructor(
     private productsService: ProductsService,
-    @Inject('API_KEY') private apiKey: string,
+    private configService: ConfigService,
   ) {}
   private counterId = 1;
   private users: User[] = [
@@ -20,7 +21,9 @@ export class UsersService {
     },
   ];
   findAll() {
-    console.log(this.apiKey);
+    const apiKey = this.configService.get('API_KEY');
+    const dbName = this.configService.get('DATABASE_NAME');
+    console.log(apiKey, dbName);
     return this.users;
   }
   findOne(id: number) {
