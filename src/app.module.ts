4612e23b4607cd093ccd854d96c403a92e353b04
copydataai <copydataai@gmail.com>
2021-10-04 16:47:12 -0400

    UseFactory, HttpModule(axios) HttpService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { HttpModule, HttpService} from '@nestjs/axios'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -8,11 +9,23 @@ const API_KEY = '1236536512';
 const API_KEY_PROD = 'PROD_KEY_ULTRA'
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [HttpModule, UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [ AppService,{
+  providers: [ {
+    provide: AppService,
+    useClass: AppService,
+  },
+  {
     provide: 'API_KEY',
     useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+  },
+  {
+    provide: 'TASKS',
+    useFactory: async (http: HttpService) => {
+      const tasks = await http.get('https://jsonplaceholder.typicode.com/todos').toPromise();
+      return tasks.data;
+    },
+    inject: [HttpService]
   }],
 })
 export class AppModule {}
