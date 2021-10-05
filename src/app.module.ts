337dbeac5fd7b68cbca33748b5a6398bf52167ed
copydataai <copydataai@gmail.com>
2021-10-05 16:24:45 -0400

    Install mongo & types/mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,40 +1,64 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule} from '@nestjs/config'
-import { HttpModule, HttpService} from '@nestjs/axios'
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 
-import * as Joi from 'joi'
+import * as Joi from 'joi';
+import { MongoClient } from 'mongodb';
 
-import { environments} from './environments'
-import config from './config'
+import { environments } from './environments';
+import config from './config';
+
+const uri = 'mongodb://root:root@localhost:27017/';
+
+const client = new MongoClient(uri);
+
+async function run() {
+  await client.connect();
+  const database = client.db('store');
+  const taskCollection = database.collection('tasks');
+  const tasks = await taskCollection.find().toArray();
+  console.log(tasks);
+}
+run();
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: environments[process.env.NODE_ENV] || '.env',
-    load: [config],
-    isGlobal: true,
-    validationSchema: Joi.object({
-      API_KEY: Joi.number().required(),
-      DATABASE_NAME: Joi.string().required(),
-      DATABASE_PORT: Joi.number().required(),
-    })
-  }), HttpModule, UsersModule, ProductsModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
+    }),
+    HttpModule,
+    UsersModule,
+    ProductsModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
-  providers: [ {
-    provide: AppService,
-    useClass: AppService,
-  },
-  {
-    provide: 'TASKS',
-    useFactory: async (http: HttpService) => {
-      const tasks = await http.get('https://jsonplaceholder.typicode.com/todos').toPromise();
-      return tasks.data;
+  providers: [
+    {
+      provide: AppService,
+      useClass: AppService,
     },
-    inject: [HttpService]
-  }],
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasks = await http
+          .get('https://jsonplaceholder.typicode.com/todos')
+          .toPromise();
+        return tasks.data;
+      },
+      inject: [HttpService],
+    },
+  ],
 })
 export class AppModule {}
